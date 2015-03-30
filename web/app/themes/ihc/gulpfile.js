59a3fe893b6645c56a7b4b43657ef2d073d321dd
Nate Beaty <nate@firebellydesign.com>
2015-03-30 14:52:47 -0500

    remove browsersync

--- web/app/themes/ihc/gulpfile.js
+++ web/app/themes/ihc/gulpfile.js
@@ -2,7 +2,6 @@
 /*global $:true*/
 var $           = require('gulp-load-plugins')();
 var argv        = require('yargs').argv;
-var browserSync = require('browser-sync');
 var gulp        = require('gulp');
 var lazypipe    = require('lazypipe');
 var merge       = require('merge-stream');
@@ -124,9 +123,6 @@ var jsTasks = function(filename) {
 var writeToManifest = function(directory) {
   return lazypipe()
     .pipe(gulp.dest, path.dist + directory)
-    .pipe(function() {
-      return $.if('**/*.{js,css}', browserSync.reload({stream:true}));
-    })
     .pipe($.rev.manifest, revManifest, {
       base: path.dist,
       merge: true
@@ -210,27 +206,12 @@ gulp.task('jshint', function() {
 gulp.task('clean', require('del').bind(null, [path.dist]));
 
 // ### Watch
-// `gulp watch` - Use BrowserSync to proxy your dev server and synchronize code
-// changes across devices. Specify the hostname of your dev server at
-// `manifest.config.devUrl`. When a modification is made to an asset, run the
-// build step for that asset and inject the changes into the page.
-// See: http://www.browsersync.io
 gulp.task('watch', function() {
-  browserSync({
-    proxy: config.devUrl,
-    snippetOptions: {
-      whitelist: ['/wp-admin/admin-ajax.php'],
-      blacklist: ['/wp-admin/**']
-    }
-  });
   gulp.watch([path.source + 'styles/**/*'], ['styles']);
   gulp.watch([path.source + 'scripts/**/*'], ['jshint', 'scripts']);
   gulp.watch([path.source + 'fonts/**/*'], ['fonts']);
   gulp.watch([path.source + 'images/**/*'], ['images']);
   gulp.watch(['bower.json', 'assets/manifest.json'], ['build']);
-  gulp.watch('**/*.php', function() {
-    browserSync.reload();
-  });
 });
 
 // ### Build
