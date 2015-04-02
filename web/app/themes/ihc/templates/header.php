be9c0cab5485ed2fad825e88f5b2ebbd7069ff09
Nate Beaty <nate@firebellydesign.com>
2015-04-02 13:56:00 -0500

    dev junk

--- web/app/themes/ihc/templates/header.php
+++ web/app/themes/ihc/templates/header.php
@@ -3,13 +3,7 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+      <h1><a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
