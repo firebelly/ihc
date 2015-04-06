686c58c6e8d71938f13c7180f05d021cdfa0272c
Nate Beaty <nate@firebellydesign.com>
2015-04-06 18:33:38 -0500

    add search form and browser warning

--- web/app/themes/ihc/templates/header.php
+++ web/app/themes/ihc/templates/header.php
@@ -1,5 +1,11 @@
 <?php use Roots\Sage\Nav\NavWalker; ?>
 
+<?php get_search_form(); ?>
+
+<!--[if lt IE 10]>
+<div class="alert alert-warning"><?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?></div>
+<![endif]-->
+
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container">
     <div class="navbar-header">
