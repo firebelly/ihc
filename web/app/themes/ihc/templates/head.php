4ed54b11c2e8884270ab5832c5bd6b53e54bda88
Nate Beaty <nate@firebellydesign.com>
2015-04-06 18:33:56 -0500

    add IE classes for old browser support

--- web/app/themes/ihc/templates/head.php
+++ web/app/themes/ihc/templates/head.php
@@ -1,9 +1,12 @@
 <!doctype html>
-<html class="no-js" <?php language_attributes(); ?>>
+<!--[if IE 8]> <html class="no-js ie8 lt-ie9 lt-ie10" lang="en"> <![endif]-->
+<!--[if IE 9 ]> <html class="no-js ie9 lt-ie10" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= esc_url(get_feed_link()); ?>">
+    <link rel="icon" type="image/png" href="<?= Roots\Sage\Assets\asset_path('images/favicon-32x32.png') ?>" />
     <?php wp_head(); ?>
   </head>
