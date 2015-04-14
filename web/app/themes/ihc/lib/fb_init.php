dd2b6aed9c5d5d122803d3cf2afc6c0f24dbf353
Nate Beaty <nate@firebellydesign.com>
2015-04-14 14:36:58 -0500

    Focus Area taxonomy page should highlight "Our Work" and nothing else (was highlighting Events)

--- web/app/themes/ihc/lib/fb_init.php
+++ web/app/themes/ihc/lib/fb_init.php
@@ -13,4 +13,19 @@ function setup() {
     'footer_links' => __('Footer Links', 'sage')
   ]);
 }
-add_action('after_setup_theme', __NAMESPACE__ . '\\setup');
+add_action('after_setup_theme', __NAMESPACE__ . '\setup');
+
+/**
+ * Custom nav highlighting for various pages
+ */
+function custom_nav_highlights($classes, $item) {
+	// Focus Area taxonomy page should highlight "Our Work" and nothing else (was highlighting Events for some reason)
+  if (is_singular('program') || is_tax('focus_area')) {
+  	if (in_array('menu-our-work', $classes))
+	    $classes[] = 'active';
+	  else
+	  	$classes = array_diff($classes, array('active'));
+	}
+  return $classes;
+}
+if (!is_admin()) { add_filter('nav_menu_css_class', __NAMESPACE__ . '\custom_nav_highlights', 20, 2); }
