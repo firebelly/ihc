397477d4d690528c04920765938bd58b160ecc1e
Margo Yoon <margo@margopixelated.com>
2015-04-21 13:49:29 -0500

    aligned search bar

--- web/app/themes/ihc/templates/searchform.php
+++ web/app/themes/ihc/templates/searchform.php
@@ -1,11 +1,21 @@
-<div class="search-toggle icon-search"></div><form role="search" method="get" class="search-form form-inline" action="<?= esc_url(home_url('/')); ?>">
-<div class="search-close"></div>
-  <label class="sr-only"><?php _e('Search for:', 'sage'); ?></label>
-  <div class="input-group">
-	<input type="search" value="<?= get_search_query(); ?>" name="s" class="search-field form-control" placeholder="Search" required>
-    	<span class="input-group-btn">
-      	<button type="submit" class="search-submit icon-search"><span class="viz-hide"><?php _e('Search', 'sage'); ?></span></button>
-    	</span>
-    </div>
-  </div>
+<div class="search-toggle">
+	<div class="icon-search"></div>
+</div>
+<form role="search" method="get" class="search-form form-inline" action="<?= esc_url(home_url('/')); ?>">
+	<div class="search-close">
+	</div>
+ 	<label class="sr-only">
+ 	<?php _e('Search for:', 'sage'); ?>
+ 	</label>
+  	<div class="input-group">
+		<input type="search" value="<?= get_search_query(); ?>" name="s" class="search-field form-control" placeholder="Search" required>
+    		<span class="input-group-btn">
+      			<button type="submit" class="search-submit icon-search">
+      				<span class="viz-hide">
+      				<?php _e('Search', 'sage'); ?>
+      				</span>
+      			</button>
+    		</span>
+    	</div>
+  	</div>
 </form>
