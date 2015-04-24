9f9d4cb88afc64d62111ebed066e33755172f47c
Nate Beaty <nate@firebellydesign.com>
2015-04-24 18:02:59 -0500

    it’s a logo! that is totally placed weird and needs massaging

--- web/app/themes/ihc/templates/header.php
+++ web/app/themes/ihc/templates/header.php
@@ -7,6 +7,7 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
 <?php get_search_form(); ?>
   <div class="container">
+  	<h1 class="logo"><a href="/">IL Humanities</a></h1>
     <nav class="site-nav" role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
