e282aa41c252db28efed08ec4f6cbd8cce92e5b0
Nate Beaty <nate@firebellydesign.com>
2015-04-30 15:06:17 -0500

    change about us page to just use body_content

--- web/app/themes/ihc/page-about-us.php
+++ web/app/themes/ihc/page-about-us.php
@@ -15,19 +15,8 @@ $body_content = apply_filters('the_content', $post->post_content);
     <h4 class="flag"><?= $content_banner_text ?></h4>
   <?php endif; ?>
   <div class="one-column">
-      <div class="column">
-        <h3 class="about-section">Board</h3>
-        <?= get_the_block('About Us Board') ?>
-      </div>
-      <div class="column expanded">
-        <h3 class="about-section">Staff & Contacts</h3>
-        <?= get_the_block('About Us Staff & Project Contacts') ?>
-      </div>
-      <div class="column">
-         <h3 class="about-section">Supporters</h3>
-        <?= get_the_block('About Us Supporters') ?>
-      </div>
-    <?= $body_content ?>  
+    <div class="entry-content user-content">
+      <?= $body_content ?>
     </div>
   </div>
 </section>
