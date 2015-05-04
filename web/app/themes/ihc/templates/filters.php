ba0069bc286ea85c6c64c036dec5701d6ea0bf23
Nate Beaty <nate@firebellydesign.com>
2015-05-04 17:55:23 -0500

    only show related programs that match future or past events based on which mode we’re in

--- web/app/themes/ihc/templates/filters.php
+++ web/app/themes/ihc/templates/filters.php
@@ -10,16 +10,35 @@ $post_type = is_post_type_archive('event') ? 'event' : 'post';
 ?>
   <form class="filters" action="/<?= $post_type == 'event' ? 'events' : 'news' ?>/" method="get">
     <div class="program-topic">Program: 
-      <input type="hidden" name="past_events" value="<?= $past_events ?>">
+      <?php if ($post_type==='event'): ?>
+        <input type="hidden" name="past_events" value="<?= $past_events ?>">
+      <?php endif ?>
       <div class="select-wrapper">
         <select name="filter_program">
           <option value="">ALL</option>
             <?php 
+            $extra_where = '';
+            // If filtering events, only match past events, or future events
+            if ($post_type=='event') {
+              $event_posts = $wpdb->get_results(
+                "SELECT ID FROM {$wpdb->posts} p
+                INNER JOIN {$wpdb->postmeta} pm ON (pm.post_id=p.iD AND pm.meta_key='_cmb2_event_start')
+                WHERE p.post_type='event' AND pm.meta_value " . (!empty($_REQUEST['past_events']) ? '<=' : '>') . time()
+              );
+              $event_ids = [];
+              foreach ($event_posts as $event)
+                $event_ids[] = $event->ID;
+              $extra_where = 'AND p2.ID IN (' . implode(',', $event_ids) . ') ';
+            }
+            if (!empty($_REQUEST['filter_focus_area'])) {
+              // todo: also filter out posts that match focus_area first
+            }
             $programs_related = $wpdb->get_results(
               "SELECT p.ID,p.post_title FROM {$wpdb->postmeta} pm 
               INNER JOIN {$wpdb->posts} p ON (p.ID=pm.meta_value) 
               INNER JOIN {$wpdb->posts} p2 ON (p2.ID=pm.post_id) 
               WHERE meta_key='_cmb2_related_program' AND p2.post_type='{$post_type}' 
+              {$extra_where}
               GROUP BY pm.meta_value ORDER BY p.post_title"
             );
             foreach ($programs_related as $program):
