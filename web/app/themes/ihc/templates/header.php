b5308b93bf11389e4115d4d8d60e3b7d946c922b
Nate Beaty <nate@firebellydesign.com>
2015-05-08 14:01:39 -0500

    cleanup, move typography to proper stylesheet, fix headers for mobile, fix article lists for mobile, remove all unnecessary z-indexes, move :hover outside mobile to avoid two-click links, remove duplicate old-browser alert

--- web/app/themes/ihc/templates/header.php
+++ web/app/themes/ihc/templates/header.php
@@ -1,9 +1,5 @@
 <?php use Roots\Sage\Nav\NavWalker; ?>
 
-<!--[if lt IE 10]>
-<div class="alert alert-warning"><?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?></div>
-<![endif]-->
-
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
 <?php get_search_form(); ?>
   <div class="container">
