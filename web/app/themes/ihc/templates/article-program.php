bbea516fb8a6a3c3fccfe7b2651dc6b7cb47e3fc
Nate Beaty <nate@firebellydesign.com>
2015-05-15 18:46:21 -0500

    make entire program listing blocks clickable

--- web/app/themes/ihc/templates/article-program.php
+++ web/app/themes/ihc/templates/article-program.php
@@ -1,6 +1,6 @@
 <article class="program">
   <div class="article-content">
-    <h2><a href="<?= get_the_permalink($program_post); ?>"><?= $program_post->post_title; ?></a></h2>
+    <h1 class="article-title"><a href="<?= get_the_permalink($program_post); ?>"><?= $program_post->post_title; ?></a></h1>
     <p><?= \Firebelly\Utils\get_excerpt($program_post); ?></p>
     <a class="button" href="<?= get_the_permalink($program_post); ?>">Learn More</a>
   </div>
