ab87933ea165965a236b9aa50f48ca733b5b3c4a
Nate Beaty <nate@firebellydesign.com>
2015-05-27 13:12:22 -0500

    typo

--- web/app/themes/ihc/assets/scripts/main.js
+++ web/app/themes/ihc/assets/scripts/main.js
@@ -291,7 +291,7 @@ var IHC = (function($) {
     if ($('.thought-of-the-day').hasClass('submitting-thought')) {
       $('.thought-of-the-day').velocity({opacity: 0, left: 50}, { easing: 'easeInSine', duration: 250,
         complete: function(e) {
-          $('.thought-of-the-day').removeClass('submitting-thought').velocity({opacity: 1, left: 0}, {  easing: 'easeOutSine', duration: 250 });
+          $('.thought-of-the-day').css('left',-50).removeClass('submitting-thought').velocity({opacity: 1, left: 0}, {  easing: 'easeOutSine', duration: 250 });
         }
       });
     }
