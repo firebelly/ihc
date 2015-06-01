e0d930f4f3e68b87506536ed8899e93249c59a5b
Nate Beaty <nate@firebellydesign.com>
2015-06-01 17:23:25 -0500

    force there to be no active nav item on search results page

--- web/app/themes/ihc/lib/fb_init.php
+++ web/app/themes/ihc/lib/fb_init.php
@@ -48,13 +48,17 @@ add_action('after_setup_theme', __NAMESPACE__ . '\setup');
  * Custom nav highlighting for various pages
  */
 function custom_nav_highlights($classes, $item) {
-	// Focus Area taxonomy page should highlight "Our Work" and nothing else (was highlighting Events for some reason)
+  // Focus Area taxonomy page should highlight "Our Work" and nothing else (was highlighting Events for some reason)
   if (is_singular('program') || is_tax('focus_area')) {
-  	if (in_array('menu-our-work', $classes))
-	    $classes[] = 'active';
-	  else
-	  	$classes = array_diff($classes, array('active'));
-	}
+    if (in_array('menu-our-work', $classes))
+      $classes[] = 'active';
+    else
+      $classes = array_diff($classes, array('active'));
+  } else if (is_search()) {
+    // no active nav on search
+    $classes = array_diff($classes, array('active'));
+  }
+
   return $classes;
 }
 if (!is_admin()) { add_filter('nav_menu_css_class', __NAMESPACE__ . '\custom_nav_highlights', 20, 2); }
