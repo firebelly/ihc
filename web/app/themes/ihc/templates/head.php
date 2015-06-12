d7ab6eab58050382265129f358b4ad6d02b136db
Nate Beaty <nate@firebellydesign.com>
2015-06-12 15:14:48 -0500

    switch to asynchronous typekit embed

--- web/app/themes/ihc/templates/head.php
+++ web/app/themes/ihc/templates/head.php
@@ -8,7 +8,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= esc_url(get_feed_link()); ?>">
     <link rel="icon" type="image/png" href="<?= Roots\Sage\Assets\asset_path('images/favicon-32x32.png') ?>" />
-    <script src="//use.typekit.net/ekm2caz.js"></script>
-    <script>try{Typekit.load();}catch(e){}</script>
+    <script>
+      (function(d) {
+        var config = {
+          kitId: 'ekm2caz',
+          scriptTimeout: 3000
+        },
+        h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+      })(document);
+    </script>
     <?php wp_head(); ?>
   </head>
