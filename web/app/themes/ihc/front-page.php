1bee16d093b2b23911a14ec27f242e73d37d780d
Nate Beaty <nate@firebellydesign.com>
2018-01-08 17:27:12 -0600

    don't show "load more events" if there's only one page

--- web/app/themes/ihc/front-page.php
+++ web/app/themes/ihc/front-page.php
@@ -30,7 +30,7 @@ $secondary_header_text = strip_tags($secondary_header_text, '<u><strong><em><a><
 <section class="focus-areas">
   <h2 class="flag">Our Focus Areas</h2>
   <ul class="focus-list">
-    <?php 
+    <?php
     $focus_areas = get_terms('focus_area');
     foreach ($focus_areas as $focus_area) {
       echo '<li><a href="' . get_term_link($focus_area) . '">' . $focus_area->name . '</a></li>';
@@ -39,7 +39,7 @@ $secondary_header_text = strip_tags($secondary_header_text, '<u><strong><em><a><
   </ul>
 </section>
 
-<?php 
+<?php
 // homepage shows all current events on map
 echo \Firebelly\PostTypes\Event\get_events(['num_posts' => -1, 'map-points' => true]);
 ?>
@@ -50,7 +50,9 @@ echo \Firebelly\PostTypes\Event\get_events(['num_posts' => -1, 'map-points' => t
     <?php echo \Firebelly\PostTypes\Event\get_events(['num_posts' => 3]); ?>
   </div>
   <div class="events-buttons">
-    <div class="load-more" data-post-type="event" data-page-at="1" data-past-events="0" data-per-page="3" data-total-pages="<?= ceil($total_events/3) ?>"><a class="no-ajaxy button" href="#">Load More</a></div>
+    <?php if (ceil($total_events/3) > 1): ?>
+      <div class="load-more" data-post-type="event" data-page-at="1" data-past-events="0" data-per-page="3" data-total-pages="<?= ceil($total_events/3) ?>"><a class="no-ajaxy button" href="#">Load More</a></div>
+    <?php endif; ?>
     <p class="view-all"><a href="/events/" class="button">View All Events</a></p>
   </div>
 </section>
@@ -58,7 +60,7 @@ echo \Firebelly\PostTypes\Event\get_events(['num_posts' => -1, 'map-points' => t
 <section class="news">
   <h2 class="flag">Blog &amp; News</h2>
   <div class="load-more-container article-list masonry">
-    <?php 
+    <?php
     // Recent Blog & News posts
     $news_posts = get_posts(['numberposts' => 4, 'category__not_in' => [9]]);
     if ($news_posts):
